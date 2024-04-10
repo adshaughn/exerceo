cb42840772672bb6d031eda1e995c80a34829bc2
adshaughn <96558619+adshaughn@users.noreply.github.com>
2024-04-09 21:44:12 -0700

    removing duplicates exercise

---
+++ length_of_last_word/length_of_last_word.py
@@ -0,0 +1,45 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+#
+# A word is a maximal
+# substring
+# consisting of non-space characters only.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+#
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+#
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+
+import re
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # step 1 - remove trailing spaces from string
+
+        cleaned_s = s.strip()
+
+        # step 2 - use regex to isolate last word
+        last_word = re.search("[A-Za-z]+$", cleaned_s)
+
+        lwo = last_word.group()
+
+        # step 3 - get lenght of last word
+
+        length_lw = len(lwo)
+
+        # step 4 - return length
+
+        return length_lw
